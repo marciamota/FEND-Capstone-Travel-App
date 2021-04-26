d29266537dcfd0026436a32c72489c6378e13e0f
Marcia <marcia.garciam@hotmail.com>
2021-04-26 18:56:17 -0500

    aspect in small screens fixed

--- __test__/endpoints.spec.js
+++ __test__/endpoints.spec.js
@@ -1,5 +1,5 @@
 import "babel-polyfill";
-// import { app } from '../src/server/endpoints'
+
 const request = require('supertest')
 const app = require("../src/server/endpoints");
 
@@ -12,10 +12,3 @@ it('should route to index.html', async done => {
   expect(res.status).toBe(200)
   done()
 })
-
-// describe("GET / ", () => {
-//   test("Server should respond", async () => {
-//       const response = await request(app).get("/")
-//       expect(response.statusCode).toBe(200);
-//   });
-// });
