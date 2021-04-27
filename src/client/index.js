2aabbdcba844cb34fd7f5e8b88208a74e30a0ed3
Marcia <marcia.garciam@hotmail.com>
2021-04-26 19:25:58 -0500

    DOMContentLoaded added

--- src/client/index.js
+++ src/client/index.js
@@ -2,7 +2,10 @@ import { generateMessage } from './js/app';
 import { titleCase } from './js/utils';
 import './styles/style.scss'
 
-document.getElementById('generate').addEventListener('click', generateMessage);
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('generate').addEventListener('click', generateMessage);
+});
+
 const today = new Date().toISOString().split('T')[0];
 document.getElementById('date').setAttribute('min', today);
 
